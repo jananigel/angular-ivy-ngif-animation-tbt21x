a3edba8494fb5face9cf2c72ebbe56f227150196
jananigel <jananigel@hotmail.com>
2020-10-30 10:42:15 +0800

    feat: disabled animation when change the page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,10 @@
-import { Component, VERSION } from "@angular/core";
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  VERSION,
+  ViewChild
+} from "@angular/core";
 import { trigger } from "@angular/animations";
 import { MOCK_DATA } from "./core/mock-data/mock-list-data";
 import { List } from "./core/interface/list.interface";
@@ -14,21 +20,28 @@ import { OPACITY_FADE_IN_OUT } from "./core/animation/opacity.animation";
     trigger("opacityAnimation", OPACITY_FADE_IN_OUT)
   ]
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
   // name = 'Angular ' + VERSION.major;
   isShowList = true;
   buttonName = "Show List";
   data = JSON.parse(JSON.stringify(MOCK_DATA));
+  isDisabledAnimation = false;
+
+  @ViewChild("listEle") listElement: ElementRef;
+
+  ngAfterViewInit() {}
 
   onBtnClick = () => {
     this.isShowList = !this.isShowList;
     this.buttonName = this.isShowList ? "Hide List" : "Show List";
   };
 
-  onListClick = (list: List) => {
+  onListClick = async (list: List) => {
     if (list.children && list.children.length) {
       // next page
-      console.log("next page");
+      await this.disabledAnimation();
+      await this.changeList(list);
+      await this.enabledAnimation();
       return;
     }
 
@@ -39,11 +52,39 @@ export class AppComponent {
     this.data = this.data.filter(data => data.id !== id);
   };
 
-  onRestoreClick = () => {
-    this.data = JSON.parse(JSON.stringify(MOCK_DATA));
+  onRestoreClick = async () => {
+    await this.disabledAnimation();
+    await this.changeList(MOCK_DATA);
+    await this.enabledAnimation();
   };
 
   trackById = (index: number, item: List): string => {
     return item.id;
+  };
+
+  private disabledAnimation = () => {
+    return new Promise(resovle => {
+      this.isDisabledAnimation = true;
+      resovle();
+    });
+  };
+
+  private changeList = (data: List | List[]) => {
+    return new Promise(resolve => {
+      setTimeout(() => {
+        const listData = "children" in data ? data.children : data;
+        this.data = JSON.parse(JSON.stringify(listData));
+        resolve();
+      });
+    });
+  };
+
+  private enabledAnimation = () => {
+    return new Promise(resolve => {
+      setTimeout(() => {
+        this.isDisabledAnimation = false;
+        resolve();
+      });
+    });
   };
 }
